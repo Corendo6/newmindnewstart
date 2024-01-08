a986ecf3936ccc13a6fc04dc0e588f2196d1e74c
Corendo6 <durrl5897@gmail.com>
2024-01-08 21:19:15 +0900

    시작이반이다

---
+++ src/main/java/totlwkr/tofhdnsakdmatotlwkr/TofhdnsakdmatotlwkrApplication.java
@@ -0,0 +1,13 @@
+package totlwkr.tofhdnsakdmatotlwkr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofhdnsakdmatotlwkrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TofhdnsakdmatotlwkrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/totlwkr/tofhdnsakdmatotlwkr/TofhdnsakdmatotlwkrApplicationTests.java
@@ -0,0 +1,13 @@
+package totlwkr.tofhdnsakdmatotlwkr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofhdnsakdmatotlwkrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
